0d02d95a4295d06c3749410fc7595f157fd6d3af
Pongter Chia <dozeuto@gmail.com>
2024-12-31 21:14:51 +0800

    Initial project.

---
+++ Famine/FamineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamineApp.swift
+//  Famine
+//
+//  Created by Pongt Chia on 31/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FamineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FamineTests/FamineTests.swift
@@ -0,0 +1,17 @@
+//
+//  FamineTests.swift
+//  FamineTests
+//
+//  Created by Pongt Chia on 31/12/24.
+//
+
+import Testing
+@testable import Famine
+
+struct FamineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
