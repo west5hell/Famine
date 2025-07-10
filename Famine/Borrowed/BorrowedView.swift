9f8b80d810c254cf20b6826d30527db04b833814
Ponter Chia <dozeuto@gmail.com>
2025-07-10 20:21:41 +0800

    feat: add BorrowedView and LentView with basic UI structure; update project settings and asset configurations

---
+++ Famine/Borrowed/BorrowedView.swift
@@ -0,0 +1,18 @@
+//
+//  BorrowedView.swift
+//  Famine
+//
+//  Created by Pongt Chia on 5/7/25.
+//
+
+import SwiftUI
+
+struct BorrowedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BorrowedView()
+}
---
+++ Famine/Lent/LentView.swift
@@ -0,0 +1,18 @@
+//
+//  LentView.swift
+//  Famine
+//
+//  Created by Pongt Chia on 5/7/25.
+//
+
+import SwiftUI
+
+struct LentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LentView()
+}
