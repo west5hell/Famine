66132620f5b2e5f55cd2739c7ab009eebe1633e6
Pongter <dozeuto@gmail.com>
2025-07-15 16:45:27 +0800

    feat: enhance ContentView with total lent and borrowed displays; improve UI layout and add debt action handling

--- Famine/ContentView.swift
+++ Famine/ContentView.swift
@@ -12,42 +12,110 @@ struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query(sort: \Debt.updatedAt, order: .reverse) private var debts: [Debt]
     @State private var selectedDebt: Debt?
+    @State private var addNewDebt: Bool = false
+    @State private var isLentTo = false
+    @State private var isBorrowedFrom = false
+
+    private var totalLent: Decimal {
+        debts
+            .filter { $0.action == .lentTo }
+            .reduce(Decimal.zero) { partialResult, debt in
+                partialResult + debt.currentAmount
+            }
+    }
+
+    private var totalBorrowed: Decimal {
+        debts
+            .filter { $0.action == .borrowedFrom }
+            .reduce(Decimal.zero) { partialResult, debt in
+                partialResult + debt.currentAmount
+            }
+    }
 
     var body: some View {
         NavigationStack {
-            ScrollView {
-                LazyVStack {
-                    ForEach(
-                        debts,
-                        content: { debt in
-                            DebtRowView(debt: debt)
-                                .onTapGesture {
-                                    selectedDebt = debt
-                                }
+            VStack {
+                Grid {
+                    GridRow {
+                        VStack {
+                            Text("Lent:")
+                                .padding(.top)
+                                .padding(.bottom, 8)
+                            Text(totalLent, format: .localCurrencySimple)
+                                .fontWeight(.heavy)
+                                .padding(.bottom)
+                        }
+                        .frame(maxWidth: .infinity)
+                        .background(Color.lent.opacity(0.6))
+                        VStack {
+                            Text("Borrowed:")
+                                .padding(.top)
+                                .padding(.bottom, 8)
+                            Text(totalBorrowed, format: .localCurrencySimple)
+                                .fontWeight(.heavy)
+                                .padding(.bottom)
+                        }
+                        .frame(maxWidth: .infinity)
+                        .background(Color.borrowed.opacity(0.6))
+                    }
+                    .font(.title2)
+                    .clipShape(.rect(cornerRadius: 10))
+                }
+                .padding(16)
+
+                ScrollView {
+                    LazyVStack {
+                        ForEach(
+                            debts,
+                            content: { debt in
+                                DebtRowView(debt: debt)
+                                    .padding(.vertical, 8)
+                                    .onTapGesture {
+                                        selectedDebt = debt
+                                    }
+                            }
+                        )
+                    }
+                    .sheet(item: $selectedDebt) { debt in
+                        DebtDetailView(debt: debt)
+                    }
+                    .sheet(
+                        isPresented: $isLentTo,
+                        content: {
+                            DebtAddView(debtAction: .lentTo)
+                                .presentationDetents([.medium])
                         }
                     )
-                }
-                .sheet(item: $selectedDebt) { debt in
-                    DebtDetailView(debt: debt)
-                }
-                .toolbar {
-                    ToolbarItem(placement: .topBarTrailing) {
-                        Button("Add", systemImage: "plus") {
-                            let james = Debt(
-                                action: .borrowedFrom,
-                                name: "James"
-                            )
-                            let borrowed = Transaction(
-                                amount: 20_000,
-                                startDate: Calendar.current.date(
-                                    byAdding: .day,
-                                    value: -5,
-                                    to: Date()
-                                )!
-                            )
-                            james.appendTransacation(borrowed)
+                    .sheet(
+                        isPresented: $isBorrowedFrom,
+                        content: {
+                            DebtAddView(debtAction: .borrowedFrom)
+                                .presentationDetents([.medium])
+                        }
+                    )
+                    .confirmationDialog(
+                        "Add New Debt",
+                        isPresented: $addNewDebt,
+                        actions: {
+                            Button("Lent To") {
+                                isLentTo.toggle()
+                            }
 
-                            modelContext.insert(james)
+                            Button("Borrowed From") {
+                                isBorrowedFrom.toggle()
+                            }
+
+                            Button("Cancel", role: .cancel) {}
+                        }
+                    )
+                    .toolbar {
+                        ToolbarItemGroup(placement: .topBarTrailing) {
+                            Button("Add", systemImage: "plus") {
+                                addNewDebt.toggle()
+                            }
+                            Button("Setting", systemImage: "gearshape") {
+
+                            }
                         }
                     }
                 }
