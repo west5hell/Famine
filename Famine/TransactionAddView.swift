81f234cc32ac2868353e0a135db90e1934bd7a44
Pongter <dozeuto@gmail.com>
2025-07-17 17:47:29 +0800

    refactor: improve layout and organization of TransactionAddView for better readability and maintainability

--- Famine/TransactionAddView.swift
+++ Famine/TransactionAddView.swift
@@ -8,38 +8,59 @@
 import SwiftUI
 
 struct TransactionAddView: View {
+    @Environment(\.dismiss) private var dismiss
     @State private var amount: Decimal = 0
     @State private var transactionDate: Date = Date()
     
+    var currentDebt: Debt
+    var transactionAction: TransactionAction
+    
     var body: some View {
-        VStack {
-            Text("Lend More")
-                .font(.largeTitle.weight(.heavy))
-            Divider()
-            LabeledContent("Amount") {
-                AmountTextField(decimalValue: $amount)
+        NavigationStack {
+            VStack {
+                Text("Lend More")
+                    .font(.largeTitle.weight(.heavy))
+                Divider()
+                LabeledContent("Amount") {
+                    AmountTextField(decimalValue: $amount)
+                }
+                .padding(.vertical)
+                Divider()
+                LabeledContent("Date") {
+                    DatePicker("", selection: $transactionDate, displayedComponents: .date)
+                }
+                .padding(.vertical)
             }
-            .padding(.vertical)
-            Divider()
-            LabeledContent("Date") {
-                DatePicker("", selection: $transactionDate, displayedComponents: .date)
-            }
-            .padding(.vertical)
-            Divider()
-            Button("Save") {
+            .font(.title2.bold())
+            .padding()
+            .background(.thinMaterial)
+            .clipShape(.rect(cornerRadius: 10))
+            .padding()
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("dismiss", systemImage: "chevron.down") {
+                        dismiss()
+                    }
+                }
                 
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Save") {
+                        let actuallyAmount = transactionAction == .increase ? amount : -amount
+                        
+                        let transaction = Transaction(amount: actuallyAmount, startDate: transactionDate, action: transactionAction)
+                        currentDebt.appendTransacation(transaction)
+                        
+                        dismiss()
+                    }
+                    .foregroundStyle(.white)
+                    .disabled(amount == 0)
+                    .buttonStyle(.borderedProminent)
+                }
             }
-            .buttonStyle(.borderedProminent)
-            .padding(.vertical)
         }
-        .font(.title2.bold())
-        .padding()
-        .background(.thinMaterial)
-        .clipShape(.rect(cornerRadius: 10))
-        .padding()
     }
 }
 
 #Preview {
-    TransactionAddView()
+    TransactionAddView(currentDebt: Debt(action: .lentTo, name: "Bosh"), transactionAction: .increase)
 }
