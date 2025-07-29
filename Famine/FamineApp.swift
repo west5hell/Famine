59af7926cd8e51a8ce9121a72d45d6d507b57ec4
Pongter <dozeuto@gmail.com>
2025-07-29 16:44:39 +0800

    add modelContainer to FamineApp

--- Famine/FamineApp.swift
+++ Famine/FamineApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct FamineApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: Debt.self)
         }
     }
 }
