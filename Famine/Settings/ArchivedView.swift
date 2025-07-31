b1aef8169bdc5904447ea08b2ca606341ec7edcb
Pongter <dozeuto@gmail.com>
2025-07-31 15:17:34 +0800

    add tap gesture to DebtDetailView

--- Famine/Settings/ArchivedView.swift
+++ Famine/Settings/ArchivedView.swift
@@ -11,15 +11,23 @@ import SwiftUI
 struct ArchivedView: View {
     @Environment(\.modelContext) private var modelContext
     @Query(sort: \Debt.updatedAt, order: .reverse) private var debts: [Debt]
+    
+    @State private var selectedDebt: Debt?
 
     var body: some View {
         NavigationStack {
             List {
-                ForEach(debts.filter { $0.status == .archived }) {
-                    DebtRowView(debt: $0)
+                ForEach(debts.filter { $0.status != .active }) { debt in
+                    DebtRowView(debt: debt)
                         .listRowSeparator(.hidden)
+                        .onTapGesture {
+                            selectedDebt = debt
+                        }
                 }
             }
+            .sheet(item: $selectedDebt, content: {
+                DebtDetailView(debt: $0)
+            })
             .listStyle(.plain)
             .navigationTitle("Archived")
         }
