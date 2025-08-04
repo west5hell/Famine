c057c6677de775faf3d67e9b8a54490764991d37
Pongter <dozeuto@gmail.com>
2025-08-04 17:03:52 +0800

    create Transaction folder

---
+++ Famine/Transaction/TransactionEditView.swift
@@ -0,0 +1,18 @@
+//
+//  TransactionEditView.swift
+//  Famine
+//
+//  Created by Pongt Chia on 4/8/25.
+//
+
+import SwiftUI
+
+struct TransactionEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransactionEditView()
+}
