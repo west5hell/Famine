17ee5719c7f567a6548c798105755190254409b9
Pongter <dozeuto@gmail.com>
2025-08-19 16:55:50 +0800

    update ui for DebtAddView

--- Famine/DebtAddView.swift
+++ Famine/DebtAddView.swift
@@ -16,76 +16,125 @@ struct DebtAddView: View {
     @State private var amountString = ""
     @State private var startDate: Date = Date()
     @State private var showAlert = false
-    
+    @State private var debtNotes = ""
+
+    @State private var showPhotos = false
+    @State private var showFiles = false
+
     var currentDebt: Debt? = nil
     var debtAction: DebtAction = .lentTo
     var transactionAction: TransactionAction = .initial
 
     var body: some View {
         VStack {
+            VStack(alignment: .leading) {
+                if !name.isEmpty {
+                    Text(
+                        debtAction == .lentTo
+                            ? "Who I lend to" : "Who I borrow from"
+                    )
+                    .bold()
+                    .foregroundStyle(.white.opacity(0.5))
+                }
+
+                TextField(
+                    "",
+                    text: $name,
+                    prompt: Text(
+                        debtAction == .lentTo
+                            ? "Who I lend to" : "Who I borrow from"
+                    ).foregroundStyle(Color.white.opacity(0.5))
+                )
+                .foregroundStyle(Color.white)
+                .font(.largeTitle.weight(.semibold))
+            }
+            .padding()
+            .background(debtAction == .lentTo ? Color.lent : Color.borrowed)
+
+            Spacer()
+
             Form {
-                
                 Section {
-                    if currentDebt == nil {
-                        LabeledContent {
-                            TextField("name", text: $name)
-                                .multilineTextAlignment(.trailing)
-                        } label: {
-                            Text("Name")
-                        }
-                    }
-                    
-                    
-                    LabeledContent {
-                        AmountTextField(decimalValue: $amount)
-                    } label: {
-                        Text("Amount")
-                    }
+                    AmountTextField(decimalValue: $amount)
+                        .font(.largeTitle.weight(.bold))
+                } header: {
+                    Text("Amount")
+                }
 
-
+                Section {
                     DatePicker(
                         "Start Date",
                         selection: $startDate,
-                        in: ...Date(),
                         displayedComponents: .date
                     )
-                } header: {
-                    switch debtAction {
-                    case .lentTo:
-                        Text("Lent To")
-                    case .borrowedFrom:
-                        Text("Borrowed From")
+                    .font(.headline)
+                    .padding(.vertical, 4)
+                }
+
+                Section {
+                    Button("Photos", systemImage: "photo.on.rectangle.angled") {
+                        showPhotos.toggle()
                     }
+                    .sheet(isPresented: $showPhotos) {
+                        PhotosView()
+                    }
+
+                    Button("Files", systemImage: "folder") {
+                        showFiles.toggle()
+                    }
+                    .sheet(isPresented: $showFiles) {
+                        FilesView()
+                    }
+
+                } header: {
+                    Text("Debt Evidences")
+                }
+
+                Section {
+                    /**
+                     ZStack(alignment: .topLeading) {
+                         if debtNotes.isEmpty {
+                             Text("Enter your note...")
+                                 .foregroundColor(.gray)
+                                 .padding(.horizontal, 8)
+                                 .padding(.vertical, 12)
+                         }
+
+                         TextEditor(text: $debtNotes)
+                             .padding(4)  // optional padding for better alignment
+                     }
+                     .frame(height: 150)  // adjust as needed
+                     */
+                    Text("You can add some notes here...")
+                        .foregroundStyle(Color.gray)
+                } header: {
+                    Text("Debt Note")
                 }
             }
             .headerProminence(.increased)
 
             Button("Save") {
-                showAlert = true
+
             }
-            .disabled(currentDebt != nil ? amount == 0 : (name.isEmpty || amount == 0))
-            .buttonStyle(.borderedProminent)
-        }
-        .alert("Save?", isPresented: $showAlert) {
-            Button("Save") {
-                save()
-            }
-            
-            Button("Cancel", role: .cancel) {}
+            .font(.largeTitle.weight(.bold))
         }
     }
-    
+
     private func save() {
         defer {
             dismiss()
         }
-        
+
         if let debt = currentDebt {
-            let transaction = Transaction(amount: amount, startDate: startDate, action: transactionAction)
+            let transaction = Transaction(
+                amount: amount,
+                startDate: startDate,
+                action: transactionAction
+            )
             debt.appendTransacation(transaction)
             return
         }
-        
+
         let debt = Debt(action: debtAction, name: name)
         let lent = Transaction(amount: amount, startDate: startDate)
         debt.appendTransacation(lent)
@@ -95,5 +144,7 @@ struct DebtAddView: View {
 }
 
 #Preview {
-    DebtAddView()
+    NavigationStack {
+        DebtAddView()
+    }
 }
---
+++ Famine/Evidences/FilesView.swift
@@ -0,0 +1,18 @@
+//
+//  FilesView.swift
+//  Famine
+//
+//  Created by Pongt Chia on 19/8/25.
+//
+
+import SwiftUI
+
+struct FilesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilesView()
+}
