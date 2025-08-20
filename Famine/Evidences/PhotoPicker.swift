a434ef023d691db102e6e76430b039ef633b27fc
Pongter <dozeuto@gmail.com>
2025-08-20 20:31:12 +0800

    update PhotosView

---
+++ Famine/Evidences/PhotoPicker.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoPicker.swift
+//  Famine
+//
+//  Created by Pongt Chia on 19/8/25.
+//
+
+import SwiftUI
+import PhotosUI
+
+//struct PhotoPicker: UIViewControllerRepresentable {
+//    func makeUIViewController(context: Context) -> PHPickerViewController {
+//        <#code#>
+//    }
+//}
--- Famine/Evidences/PhotosView.swift
+++ Famine/Evidences/PhotosView.swift
@@ -5,11 +5,83 @@
 //  Created by Pongt Chia on 19/8/25.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct PhotosView: View {
+    @Environment(\.dismiss) private var dismiss
+
+    @State private var selectedItems: [PhotosPickerItem] = []
+    @State private var selectedImages: [Image] = []
+    @State private var isPickerPresented = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Group {
+                if selectedImages.isEmpty {
+                    ContentUnavailableView {
+                        Label(
+                            "No Photos",
+                            systemImage: "photo.on.rectangle.angled"
+                        )
+                    } description: {
+                        Text("You don't have any photos yet.")
+                    } actions: {
+                        PhotosPicker(
+                            "Select Photos",
+                            selection: $selectedItems,
+                            matching: .images
+                        )
+                        .onChange(of: selectedItems) { oldValue, newValue in
+                            Task {
+                                selectedImages = []
+                                for item in newValue {
+                                    if let data =
+                                        try? await item.loadTransferable(
+                                            type: Data.self
+                                        ), let uiImage = UIImage(data: data)
+                                    {
+                                        selectedImages.append(
+                                            Image(uiImage: uiImage)
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    ScrollView {
+                        LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))])
+                        {
+                            ForEach(selectedImages.indices, id: \.self) { index in
+                                selectedImages[index]
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                    .clipShape(
+                                        RoundedRectangle(cornerRadius: 8)
+                                    )
+                            }
+                        }
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("dismiss", systemImage: "chevron.down") {
+                        dismiss()
+                    }
+                }
+                
+                if !selectedImages.isEmpty {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button("Add", systemImage: "plus") {
+                            
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
